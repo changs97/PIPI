a908af264bdf87f68d0f3359e164c3a23b563a5a
changs97 <pck4949@naver.com>
2021-11-10 18:34:09 +0900

    Create MainActivity.kt

---
+++ app/src/main/java/com/changs/pipi/src/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.changs.pipi.src.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.changs.pipi.R
+import com.changs.pipi.config.BaseActivity
+import com.changs.pipi.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}

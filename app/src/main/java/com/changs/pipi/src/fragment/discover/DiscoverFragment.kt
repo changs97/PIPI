eba86fdc70ebc106a493cd4cdf26907f3f0c43ac
changs97 <pck4949@naver.com>
2021-11-14 15:53:13 +0900

    Update DiscoverFragment.kt

--- app/src/main/java/com/changs/pipi/src/fragment/discover/DiscoverFragment.kt
+++ app/src/main/java/com/changs/pipi/src/fragment/discover/DiscoverFragment.kt
@@ -5,12 +5,23 @@ import android.view.View
 import com.changs.pipi.R
 import com.changs.pipi.config.BaseFragment
 import com.changs.pipi.databinding.FragmentDiscoverBinding
+import com.changs.pipi.src.fragment.home.HomeFragment
+import com.changs.pipi.src.fragment.plus.PlusFragment
+import com.changs.pipi.src.fragment.search.SearchFragment
 
 class DiscoverFragment : BaseFragment<FragmentDiscoverBinding>(FragmentDiscoverBinding::bind, R.layout.fragment_discover) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        
+        val bottomNav = binding.bottomNav
+
+        val homeFragment = HomeFragment()
+        val plusFragment = PlusFragment()
+        val searchFragment = SearchFragment()
+
+        bottomNav
+
+
     }
 }

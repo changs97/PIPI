275f992a8f85ac2557e78dbfa57aa0041c340a6d
changs97 <pck4949@naver.com>
2021-11-14 21:04:18 +0900

    Update SearchFragment.kt

--- app/src/main/java/com/pipix/pipi/src/fragment/search/SearchFragment.kt
+++ app/src/main/java/com/pipix/pipi/src/fragment/search/SearchFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.View
 import com.pipix.pipi.R
 import com.pipix.pipi.config.BaseFragment
-import com.pipix.pipi.databinding.FragmentInsertBinding
+import com.pipix.pipi.databinding.FragmentSearchBinding
 
-class SearchFragment : BaseFragment<FragmentInsertBinding>(FragmentInsertBinding::bind, R.layout.fragment_insert) {
+class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::bind, R.layout.fragment_search) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }

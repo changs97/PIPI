7c9b1338fc9af6ca59ada650e87d9234512c4568
changs97 <pck4949@naver.com>
2021-11-14 21:04:05 +0900

    Create DiscoverFragment.kt

---
+++ app/src/main/java/com/pipix/pipi/src/discover/DiscoverFragment.kt
@@ -0,0 +1,19 @@
+package com.pipix.pipi.src.discover
+
+import android.os.Bundle
+import android.view.View
+import com.pipix.pipi.R
+import com.pipix.pipi.config.BaseFragment
+import com.pipix.pipi.databinding.FragmentDiscoverBinding
+
+class DiscoverFragment : BaseFragment<FragmentDiscoverBinding>(FragmentDiscoverBinding::bind, R.layout.fragment_discover) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+
+
+    }
+}

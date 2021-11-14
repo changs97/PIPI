3df914478acea5086017a365ccf11ddfc8cf2ef8
changs97 <pck4949@naver.com>
2021-11-14 21:04:13 +0900

    Update LoginFragment.kt

--- app/src/main/java/com/pipix/pipi/src/fragment/logged_out/login/LoginFragment.kt
+++ app/src/main/java/com/pipix/pipi/src/fragment/logged_out/login/LoginFragment.kt
@@ -2,6 +2,7 @@ package com.pipix.pipi.src.fragment.logged_out.login
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.pipix.pipi.R
 import com.pipix.pipi.config.BaseFragment
 import com.pipix.pipi.databinding.FragmentLoginBinding
@@ -12,6 +13,9 @@ class LoginFragment  : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::
         super.onViewCreated(view, savedInstanceState)
 
         val back = binding.loginImgbtnBack
+
+        val t = binding.loginBtnLogin
+        t.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_discoverActiviy) }
 
 
     }

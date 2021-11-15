4722c0263d058deed4c22dd09a4c875932f02caa
changs97 <pck4949@naver.com>
2021-11-15 16:58:21 +0900

    Update DiscoverFragment.kt

--- app/src/main/java/com/pipix/pipi/src/discover/DiscoverFragment.kt
+++ app/src/main/java/com/pipix/pipi/src/discover/DiscoverFragment.kt
@@ -2,6 +2,8 @@ package com.pipix.pipi.src.discover
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.pipix.pipi.R
 import com.pipix.pipi.config.BaseFragment
 import com.pipix.pipi.databinding.FragmentDiscoverBinding
@@ -10,6 +12,13 @@ class DiscoverFragment : BaseFragment<FragmentDiscoverBinding>(FragmentDiscoverB
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val navHostFragment = activity?.supportFragmentManager?.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        // 바텀 네비게이션 뷰 와 네비게이션을 묶어준다
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
 
 
 

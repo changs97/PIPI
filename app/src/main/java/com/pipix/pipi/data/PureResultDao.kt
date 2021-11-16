6e91124bd90b857196a82826033eac587da0a964
yonghanJu <ju990828@naver.com>
2021-11-17 00:13:48 +0900

    Update PureResultDao.kt

--- app/src/main/java/com/pipix/pipi/data/PureResultDao.kt
+++ app/src/main/java/com/pipix/pipi/data/PureResultDao.kt
@@ -5,12 +5,22 @@ import androidx.room.*
 
 @Dao
 interface PureResultDao {
+
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun addPureResult(pr: PureResult)
 
     @Delete
     suspend fun deletePureResult(pr: PureResult)
 
-    @Query("SELECT * FROM result_table ORDER BY id ASC")
-    fun readAllData(): LiveData<List<PureResult>>
+    @Query("SELECT * FROM pure_result_table ORDER BY id ASC")
+    fun readAllPureData(): LiveData<List<PureResult>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addSpeechResult(sr: SpeechResult)
+
+    @Delete
+    suspend fun deleteSpeechResult(sr: SpeechResult)
+
+    @Query("SELECT * FROM speech_result_table ORDER BY id ASC")
+    fun readAllSpeechData(): LiveData<List<SpeechResult>>
 }

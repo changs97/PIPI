bba443110fa535fa5efeb0e9c219fb85abf4668c
yonghanJu <ju990828@naver.com>
2021-11-17 00:13:46 +0900

    Update PureResult.kt

--- app/src/main/java/com/pipix/pipi/data/PureResult.kt
+++ app/src/main/java/com/pipix/pipi/data/PureResult.kt
@@ -5,36 +5,52 @@ import androidx.room.PrimaryKey
 import java.io.Serializable
 
 
-@Entity(tableName = "result_table")
+@Entity(tableName = "pure_result_table")
 data class PureResult (
     @PrimaryKey(autoGenerate = true)
     val id: Int,
-    val testType: Int,
+    val tpaRight: Int,
+    val tpaLeft: Int,
+    val date: String,
+    
+    val R_250: Int,
+    val R_500: Int,
+    val R_1000: Int,
+    val R_2000: Int,
+    val R_4000: Int,
+    val R_8000: Int,
+
+    val L_250: Int,
+    val L_500: Int,
+    val L_1000: Int,
+    val L_2000: Int,
+    val L_4000: Int,
+    val L_8000: Int,
+    val personId: Int
+) : Serializable
+
+@Entity(tableName = "speech_result_table")
+data class SpeechResult(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
     val tpaRight: Int,
     val tpaLeft: Int,
     val date: String,
 
-    val scoreRight: Int? = null,
-    val scoreLeft: Int? = null,
+    val scoreRight: Int,
+    val scoreLeft: Int,
+    val personId: Int
+)
 
-    
-    val R_250: Int? = null,
-    val R_500: Int? = null,
-    val R_1000: Int? = null,
-    val R_2000: Int? = null,
-    val R_4000: Int? = null,
-    val R_8000: Int? = null,
-
-    val L_250: Int? = null,
-    val L_500: Int? = null,
-    val L_1000: Int? = null,
-    val L_2000: Int? = null,
-    val L_4000: Int? = null,
-    val L_8000: Int? = null
-) : Serializable
-
-@Entity(tableName = "asd")
-data class Asd(
+@Entity(tableName = "person_table")
+data class Person(
     @PrimaryKey(autoGenerate = true)
-    val id: Int
+    val id: Int,
+    val tpaRight: Int,
+    val tpaLeft: Int,
+    val date: String,
+
+    val scoreRight: Int,
+    val scoreLeft: Int,
+    val personId: Int
 )

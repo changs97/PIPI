b5987e2076e0f70b203a5b3814f9bc5491a8a64a
yonghanJu <ju990828@naver.com>
2021-11-17 00:13:39 +0900

    Update PRDatabase.kt

--- app/src/main/java/com/pipix/pipi/data/PRDatabase.kt
+++ app/src/main/java/com/pipix/pipi/data/PRDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [PureResult::class], version = 1, exportSchema = false)
+@Database(entities = [PureResult::class, SpeechResult::class], version = 1, exportSchema = false)
 abstract class PRDatabase: RoomDatabase() {
 
     abstract fun pureResultDao(): PureResultDao

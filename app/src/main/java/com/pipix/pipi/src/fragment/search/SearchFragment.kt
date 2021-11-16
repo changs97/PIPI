5ba70bfbe94cc68d647f555fb27968d1cb8b9c06
changs97 <pck4949@naver.com>
2021-11-16 18:49:04 +0900

    Create SearchFragment.kt

---
+++ app/src/main/java/com/pipix/pipi/src/fragment/search/SearchFragment.kt
@@ -0,0 +1,14 @@
+package com.pipix.pipi.src.fragment.search
+
+import android.os.Bundle
+import android.view.View
+import com.pipix.pipi.R
+import com.pipix.pipi.config.BaseFragment
+import com.pipix.pipi.databinding.FragmentSearchBinding
+
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::bind, R.layout.fragment_search) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

d4c2985903ef4fc937f4ae14ea6639bc62aede45
yonghanJu <ju990828@naver.com>
2021-11-17 20:18:39 +0900

    Update PureResult.kt

--- app/src/main/java/com/pipix/pipi/data/PureResult.kt
+++ app/src/main/java/com/pipix/pipi/data/PureResult.kt
@@ -1,18 +1,21 @@
 package com.pipix.pipi.data
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.io.Serializable
+import java.util.*
 
 
 @Entity(tableName = "pure_result_table")
 data class PureResult (
-    @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    @PrimaryKey
+    val oldId: Int,
+    @PrimaryKey
+    val date: Date,
     val tpaRight: Int,
     val tpaLeft: Int,
-    val date: String,
-    
+
     val R_250: Int,
     val R_500: Int,
     val R_1000: Int,
@@ -25,25 +28,39 @@ data class PureResult (
     val L_1000: Int,
     val L_2000: Int,
     val L_4000: Int,
-    val L_8000: Int,
-    val personId: Int
+    val L_8000: Int
 ) : Serializable
+
+
+@Entity(tableName = "old_table")
+data class Old(
+    @PrimaryKey(autoGenerate = true)
+    val oldId: Int,
+    val oldName: String,
+    val oldAge: Int,
+    val oldSex: Int,
+    val oldAddress: String,
+    val oldImage: String,
+
+    // not Foreign Key
+    val userId: String
+)
+
+@Entity(tableName = "schedule_table")
+data class Schedule(
+    // not key
+    val userId: String,
+    @PrimaryKey
+    @ForeignKey(entity = Old::class, parentColumns = arrayOf("oldId"), onDelete = ForeignKey.CASCADE)
+    val oldId: Int,
+    @PrimaryKey
+    val day: Int,
+    val startTime: String,
+    val endTime: String
+)
 
 @Entity(tableName = "speech_result_table")
 data class SpeechResult(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int,
-    val tpaRight: Int,
-    val tpaLeft: Int,
-    val date: String,
-
-    val scoreRight: Int,
-    val scoreLeft: Int,
-    val personId: Int
-)
-
-@Entity(tableName = "person_table")
-data class Person(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val tpaRight: Int,

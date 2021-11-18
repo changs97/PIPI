c8fe6c63ddb8ebe562ca718f3e827708d578819b
yonghanJu <ju990828@naver.com>
2021-11-19 01:57:06 +0900

    Update SearchAdapter.kt

--- app/src/main/java/com/pipix/pipi/src/fragment/search/SearchAdapter.kt
+++ app/src/main/java/com/pipix/pipi/src/fragment/search/SearchAdapter.kt
@@ -18,14 +18,16 @@ class SearchAdapter(private val oldList: MutableList<Old>)  :  RecyclerView.Adap
 
         var name : TextView? = null
         var image: ImageView? = null
-        var detail: TextView? = null
+        var address: TextView? = null
+        var schedule: TextView? = null
         lateinit var old: Old
 
         init {
             // Define click listener for the ViewHolder's View.
             name = view.findViewById(R.id.search_card_name)
-            detail = view.findViewById(R.id.search_card_detail)
+            address = view.findViewById(R.id.search_card_address)
             image = view.findViewById(R.id.search_card_image)
+            schedule = view.findViewById(R.id.search_card_schedule)
 
             // 객체 넘겨주기
             view.setOnClickListener {
@@ -47,7 +49,8 @@ class SearchAdapter(private val oldList: MutableList<Old>)  :  RecyclerView.Adap
 
         holder.name!!.text = currentItem.oldName + " " + currentItem.oldID
         holder.image!!.setImageResource(R.drawable.ic_basic_profile)
-        holder.detail!!.text = currentItem.oldAddress
+        holder.address!!.text = currentItem.oldAddress
+        holder.schedule!!.text = getSchedule(currentItem)
         holder.old = oldList[position]
     }
 
@@ -55,4 +58,36 @@ class SearchAdapter(private val oldList: MutableList<Old>)  :  RecyclerView.Adap
         return oldList.size
     }
 
+    fun getSchedule(old: Old): String{
+        var schedule = ""
+        if(!old.mon.isNullOrBlank()){
+            val sl = old.mon.split("-")
+            schedule += "월요일 ${sl[0]}:${sl[1]} - ${sl[2]}:${sl[3]} "
+        }
+        if(!old.tue.isNullOrBlank()){
+            val sl = old.tue.split("-")
+            schedule += "화요일 ${sl[0]}:${sl[1]} - ${sl[2]}:${sl[3]} "
+        }
+        if(!old.wed.isNullOrBlank()){
+            val sl = old.wed.split("-")
+            schedule += "화요일 ${sl[0]}:${sl[1]} - ${sl[2]}:${sl[3]} "
+        }
+        if(!old.thu.isNullOrBlank()){
+            val sl = old.thu.split("-")
+            schedule += "목요일 ${sl[0]}:${sl[1]} - ${sl[2]}:${sl[3]} "
+        }
+        if(!old.fri.isNullOrBlank()){
+            val sl = old.fri.split("-")
+            schedule += "금요일 ${sl[0]}:${sl[1]} - ${sl[2]}:${sl[3]} "
+        }
+        if(!old.sat.isNullOrBlank()){
+            val sl = old.sat.split("-")
+            schedule += "토요일 ${sl[0]}:${sl[1]} - ${sl[2]}:${sl[3]} "
+        }
+        if(!old.sun.isNullOrBlank()){
+            val sl = old.sun.split("-")
+            schedule += "일요일 ${sl[0]}:${sl[1]} - ${sl[2]}:${sl[3]} "
+        }
+        return schedule
+    }
 }

cc3f3b3ef47e56ae661c85ee511de72cdd9b4d5f
changs97 <pck4949@naver.com>
2021-11-19 04:27:48 +0900

    Update InsertFragment.kt

--- app/src/main/java/com/pipix/pipi/src/fragment/insertPerson/InsertFragment.kt
+++ app/src/main/java/com/pipix/pipi/src/fragment/insertPerson/InsertFragment.kt
@@ -1,9 +1,9 @@
 package com.pipix.pipi.src.fragment.insertPerson
 
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.pipix.pipi.R
@@ -15,13 +15,19 @@ import com.pipix.pipi.src.main.MainActivity
 
 
 class InsertFragment : BaseFragment<FragmentInsertBinding>(FragmentInsertBinding::bind, R.layout.fragment_insert) {
-
-
     companion object {
         var dataList = mutableListOf<TestData>()
         var recyclerviewAdapter = InsertAdapter(dataList)
 
         // 방문 요일의 체크 상태를 처리할 변수
+        var monChecked = false
+        var tuesChecked  = false
+        var wedChecked  = false
+        var thuChecked = false
+        var friChecked = false
+        var satChecked = false
+        var sunChecked  = false
+
         var monTime : String? = null
         var tuesTime : String? = null
         var wedTime : String? = null
@@ -39,7 +45,7 @@ class InsertFragment : BaseFragment<FragmentInsertBinding>(FragmentInsertBinding
         val address = binding.insertEdittextAddress
         val age = binding.insertEdittextAge
         val radioGroup = binding.insertRadiogroup
-        var sexType : Int? = null
+        var genderType : Int? = null
 
         val BtnMon = binding.insertMon
         val BtnTues = binding.insertTues
@@ -49,33 +55,112 @@ class InsertFragment : BaseFragment<FragmentInsertBinding>(FragmentInsertBinding
         val BtnSat = binding.insertSat
         val BtnSun = binding.insertSun
 
+        val swipeLayout = binding.insertSwiperefreshlayot
+
+        fun refresh() {
+            swipeLayout.post { swipeLayout.isRefreshing = true
+                BtnMon.isChecked = monChecked
+                BtnTues.isChecked =  tuesChecked
+                BtnWed.isChecked = wedChecked
+                BtnThu.isChecked = thuChecked
+                BtnFri.isChecked = friChecked
+                BtnSat.isChecked = satChecked
+                BtnSun.isChecked = sunChecked
+                swipeLayout.isRefreshing = false
+            }}
+
+
+
+
+        swipeLayout.setOnRefreshListener {
+            BtnMon.isChecked = monChecked
+            BtnTues.isChecked =  tuesChecked
+            BtnWed.isChecked = wedChecked
+            BtnThu.isChecked = thuChecked
+            BtnFri.isChecked = friChecked
+            BtnSat.isChecked = satChecked
+            BtnSun.isChecked = sunChecked
+            swipeLayout.isRefreshing = false
+        }
+
+
+
+
+
+
+
         radioGroup.setOnCheckedChangeListener { group, checkedId ->
             when (checkedId) {
-                R.id.insert_man-> sexType = 1
-                R.id.insert_woman-> sexType = 2
+                R.id.insert_man-> genderType = 1
+                R.id.insert_woman-> genderType = 2
             }
         }
 
-        BtnMon.setOnClickListener{ CustomDialog(context as MainActivity, "월요일").show()}
-        BtnTues.setOnClickListener{ CustomDialog(context as MainActivity, "화요일").show()}
-        BtnWed.setOnClickListener{ CustomDialog(context as MainActivity, "수요일").show()}
-        BtnThu.setOnClickListener{ CustomDialog(context as MainActivity, "목요일").show()}
-        BtnFri.setOnClickListener{ CustomDialog(context as MainActivity, "금요일").show()}
-        BtnSat.setOnClickListener{ CustomDialog(context as MainActivity, "토요일").show()}
-        BtnSun.setOnClickListener{ CustomDialog(context as MainActivity, "일요일").show()}
+
+
+
+        BtnMon.setOnClickListener{
+            if(BtnMon.isChecked) {
+                CustomDialog(context as MainActivity, "월요일").show()
+                monChecked = true
+            } else BtnMon.isChecked = true
+            refresh()}
+        BtnTues.setOnClickListener{
+            if(BtnTues.isChecked){
+                CustomDialog(context as MainActivity, "화요일").show()
+                tuesChecked = true
+            } else BtnTues.isChecked = true
+            refresh()}
+        BtnWed.setOnClickListener{
+            if(BtnWed.isChecked){
+                CustomDialog(context as MainActivity, "수요일").show()
+                wedChecked = true
+            } else BtnWed.isChecked = true
+            refresh()}
+        BtnThu.setOnClickListener{
+            if(BtnThu.isChecked){
+                CustomDialog(context as MainActivity, "목요일").show()
+                thuChecked = true
+            } else BtnThu.isChecked = true
+            refresh()}
+        BtnFri.setOnClickListener{
+            if(BtnFri.isChecked){
+                CustomDialog(context as MainActivity, "금요일").show()
+                friChecked =true
+            } else BtnFri.isChecked = true
+            refresh()}
+        BtnSat.setOnClickListener{
+            if(BtnSat.isChecked){
+                CustomDialog(context as MainActivity, "토요일").show()
+                satChecked = true
+            } else BtnSat.isChecked = true
+            refresh()}
+        BtnSun.setOnClickListener{
+            if(BtnSun.isChecked){
+                CustomDialog(context as MainActivity, "일요일").show()
+                sunChecked = true
+            } else BtnSun.isChecked = true
+            refresh()}
 
         complete.setOnClickListener {
-            if(name.text != null && age.text != null && sexType != null && address.text != null){
+            if(name.text != null && age.text != null && genderType != null && address.text != null){
                 MainActivity.viewModel.addOld(
-                    Old(0, "userID", name.text.toString(), age.text.toString().toInt(), sexType!! ,address.text.toString(), null,
+                    Old(0, "userID", name.text.toString(), age.text.toString().toInt(), genderType!! ,address.text.toString(), null,
                         monTime, tuesTime, wedTime, thuTime, friTime, satTime, sunTime))
-                showCustomToast(MainActivity.viewModel.readAllOld.toString())
+
                 dataList.clear()
                 recyclerviewAdapter.notifyDataSetChanged()
                 name.text = null
                 age.text = null
                 address.text = null
-                sexType = null
+                genderType = null
+                monChecked = false
+                tuesChecked  = false
+                wedChecked  = false
+                thuChecked = false
+                friChecked = false
+                satChecked = false
+                sunChecked  = false
                 radioGroup.clearCheck()
 
             }
@@ -93,6 +178,7 @@ class InsertFragment : BaseFragment<FragmentInsertBinding>(FragmentInsertBinding
             }
         })
 
+
         recyclerView.adapter = recyclerviewAdapter
     }
 
@@ -100,6 +186,7 @@ class InsertFragment : BaseFragment<FragmentInsertBinding>(FragmentInsertBinding
         super.onDetach()
         dataList.clear()
     }
+
 
 
 

598eae43e8444f2cac7f22ce995b1c2274c724c5
changs97 <pck4949@naver.com>
2021-11-18 14:00:04 +0900

    Update InsertFragment.kt

--- app/src/main/java/com/pipix/pipi/src/fragment/insertPerson/InsertFragment.kt
+++ app/src/main/java/com/pipix/pipi/src/fragment/insertPerson/InsertFragment.kt
@@ -2,6 +2,7 @@ package com.pipix.pipi.src.fragment.insertPerson
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pipix.pipi.R
 import com.pipix.pipi.config.BaseFragment
 import com.pipix.pipi.databinding.FragmentInsertBinding
@@ -10,5 +11,20 @@ class InsertFragment : BaseFragment<FragmentInsertBinding>(FragmentInsertBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val recyclerView = binding.insertRecyclerview
+        recyclerView.setLayoutManager(object : LinearLayoutManager(activity){
+            override fun canScrollVertically(): Boolean {
+                return false
+            }
+        })
+
+        var testData = mutableListOf<String>()
+
+
+
+
+        // 리사이클러뷰에 Adapter 객체 지정.
+        val adapter = InsertAdapter(testData)
+        recyclerView.adapter = adapter
     }
 }

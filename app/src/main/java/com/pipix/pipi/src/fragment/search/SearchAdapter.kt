873bd6bd2217e7ecd81899973a86641ffb7819a7
yonghanJu <ju990828@naver.com>
2021-11-19 01:12:37 +0900

    Update SearchAdapter.kt

--- app/src/main/java/com/pipix/pipi/src/fragment/search/SearchAdapter.kt
+++ app/src/main/java/com/pipix/pipi/src/fragment/search/SearchAdapter.kt
@@ -1,13 +1,16 @@
 package com.pipix.pipi.src.fragment.search
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.pipix.pipi.R
 import com.pipix.pipi.data.Old
+import com.pipix.pipi.src.main.MainActivity
 
 class SearchAdapter(private val oldList: MutableList<Old>)  :  RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
 
@@ -15,11 +18,20 @@ class SearchAdapter(private val oldList: MutableList<Old>)  :  RecyclerView.Adap
 
         var name : TextView? = null
         var image: ImageView? = null
+        var detail: TextView? = null
+        lateinit var old: Old
 
         init {
             // Define click listener for the ViewHolder's View.
             name = view.findViewById(R.id.search_card_name)
+            detail = view.findViewById(R.id.search_card_detail)
             image = view.findViewById(R.id.search_card_image)
+
+            // 객체 넘겨주기
+            view.setOnClickListener {
+                MainActivity.viewModel.currentOld = old
+                findNavController(view).navigate(R.id.action_searchFragment_to_profileFragment)
+            }
         }
     }
 
@@ -34,6 +46,9 @@ class SearchAdapter(private val oldList: MutableList<Old>)  :  RecyclerView.Adap
         var currentItem = oldList[position]
 
         holder.name!!.text = currentItem.oldName + " " + currentItem.oldID
+        holder.image!!.setImageResource(R.drawable.ic_basic_profile)
+        holder.detail!!.text = currentItem.oldAddress
+        holder.old = oldList[position]
     }
 
     override fun getItemCount(): Int {

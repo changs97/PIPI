c22ed4468ef7ac92815062be731419f6b47c53c7
changs97 <pck4949@naver.com>
2021-11-22 22:40:47 +0900

    Update BaseFragment.kt

--- app/src/main/java/com/pipix/pipi/config/BaseFragment.kt
+++ app/src/main/java/com/pipix/pipi/config/BaseFragment.kt
@@ -1,5 +1,6 @@
 package com.pipix.pipi.config
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
@@ -10,6 +11,7 @@ import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import com.pipix.pipi.util.LoadingDialog
 
 // Fragment의 기본을 작성, 뷰 바인딩 활용
 abstract class BaseFragment<B : ViewBinding>(
@@ -18,6 +20,7 @@ abstract class BaseFragment<B : ViewBinding>(
 ) : Fragment(layoutResId) {
     private var _binding: B? = null
     protected val binding get() = _binding!!
+    lateinit var mLoadingDialog: LoadingDialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +38,17 @@ abstract class BaseFragment<B : ViewBinding>(
 
     fun showCustomToast(message: String) {
         Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun showLoadingDialog(context: Context) {
+        mLoadingDialog = LoadingDialog(context)
+        mLoadingDialog.show()
+    }
+
+    fun dismissLoadingDialog() {
+        if (mLoadingDialog.isShowing) {
+            mLoadingDialog.dismiss()
+        }
     }
 
 

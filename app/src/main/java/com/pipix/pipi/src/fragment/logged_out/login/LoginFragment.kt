e6ced150c77ff4b6bb248dfff89e195a167dda4b
changs97 <pck4949@naver.com>
2021-12-02 23:45:29 +0900

    Update LoginFragment.kt

--- app/src/main/java/com/pipix/pipi/src/fragment/logged_out/login/LoginFragment.kt
+++ app/src/main/java/com/pipix/pipi/src/fragment/logged_out/login/LoginFragment.kt
@@ -6,12 +6,9 @@ import android.view.View
 import androidx.navigation.fragment.findNavController
 import com.pipix.pipi.R
 import com.pipix.pipi.config.ApplicationClass
-import com.pipix.pipi.config.ApplicationClass.Companion.sSharedPreferences
 import com.pipix.pipi.config.BaseFragment
 import com.pipix.pipi.data.Webservice
 import com.pipix.pipi.databinding.FragmentLoginBinding
-import com.pipix.pipi.src.fragment.logged_out.join.register_step2.model.SignUpBody
-import com.pipix.pipi.src.fragment.logged_out.join.register_step2.model.SignUpResponse
 import com.pipix.pipi.src.fragment.logged_out.login.model.LoginBody
 import com.pipix.pipi.src.fragment.logged_out.login.model.LoginResponse
 import retrofit2.Call
@@ -37,18 +34,6 @@ class LoginFragment  : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::
 
             tryGetLogin(LoginBody(userID.text.toString(), password.text.toString()))
 
-            var id = ""
-            id = if(userID.text.isNullOrBlank()) "userID" else userID.text.toString()
-            with (sSharedPreferences.edit()) {
-                putString(getString(R.string.sharedIDKey), id)
-                commit()
-            }
-
-
-
-
-            findNavController().navigate(R.id.action_loginFragment_to_second_graph)
-            findNavController().graph.startDestination = R.id.second_graph
         }
 
 
@@ -62,12 +47,19 @@ class LoginFragment  : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::
                 call: Call<LoginResponse>,
                 response: Response<LoginResponse>
             ) { Log.d("TEST_tryGetLogin",response.body().toString())
-                //환자 리스트 반환
+                val data = response.body() as LoginResponse
+
+                ApplicationClass.prefs.userId = data.id.toString()
+                ApplicationClass.prefs.userName = data.name
+
+                findNavController().navigate(R.id.action_loginFragment_to_second_graph)
+                findNavController().graph.startDestination = R.id.second_graph
 
             }
 
             override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
                 Log.d("TEST_tryGetLogin",t.message ?:"통신 오류")
+                showCustomToast("로그인 실패")
             }
         })
     }

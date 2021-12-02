12968c2c9aedef9c2f1ba479cf49a12834726ca1
changs97 <pck4949@naver.com>
2021-12-02 21:27:27 +0900

    Update RegisterStep2Fragment.kt

--- app/src/main/java/com/pipix/pipi/src/fragment/logged_out/join/register_step2/RegisterStep2Fragment.kt
+++ app/src/main/java/com/pipix/pipi/src/fragment/logged_out/join/register_step2/RegisterStep2Fragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.pipix.pipi.R
 import com.pipix.pipi.config.ApplicationClass
 import com.pipix.pipi.config.BaseFragment
 import com.pipix.pipi.data.Webservice
 import com.pipix.pipi.databinding.FragmentRegisterStep2Binding
+import com.pipix.pipi.src.fragment.chart.ChartFragmentArgs
 import com.pipix.pipi.src.fragment.logged_out.join.register_step2.model.SignUpBody
 import com.pipix.pipi.src.fragment.logged_out.join.register_step2.model.SignUpResponse
 import retrofit2.Call
@@ -18,18 +20,24 @@ import retrofit2.Response
 class RegisterStep2Fragment : BaseFragment<FragmentRegisterStep2Binding>(
     FragmentRegisterStep2Binding::bind, R.layout.fragment_register_step2) {
 
+    val args: RegisterStep2FragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val userName = binding.registerStep2EdittextName
         val back = binding.registerStep2ImgbtnBack
+
+        val userId = args.resisterArg.userId
+        val password = args.resisterArg.password
+
         back.setOnClickListener {
             findNavController().popBackStack()
         }
         val t = binding.registerStep2BtnJoin
         t.setOnClickListener {
-            findNavController().navigate(R.id.action_registerStep2Fragment_to_second_graph)
-            findNavController().graph.startDestination = R.id.second_graph
+
+            tryPostSignUp(SignUpBody(userId,userName.text.toString(),password))
 
             var name = ""
             name = if(userName.text.isNullOrBlank()) "userName" else userName.text.toString()
@@ -37,8 +45,15 @@ class RegisterStep2Fragment : BaseFragment<FragmentRegisterStep2Binding>(
                 putString(getString(R.string.sharedUserNameKey), name)
                 commit()
             }
+            Log.d("TEST_tryPostSignUp",userId+userName.text.toString()+password)
+
+
+            findNavController().navigate(R.id.action_registerStep2Fragment_to_second_graph)
+            findNavController().graph.startDestination = R.id.second_graph
+
         }
     }
+
 
     fun tryPostSignUp(body : SignUpBody){
         val UploadRetrofitInterface = ApplicationClass.sRetrofit.create(Webservice::class.java)
@@ -47,7 +62,7 @@ class RegisterStep2Fragment : BaseFragment<FragmentRegisterStep2Binding>(
             override fun onResponse(
                 call: Call<SignUpResponse>,
                 response: Response<SignUpResponse>
-            ) { //response.body()
+            ) { Log.d("TEST_tryPostSignUp",response.body().toString()+"OnResponse")
 
             }
 

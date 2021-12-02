aa3dbcc3cc93cd9f137c25926ae8218118225d85
changs97 <pck4949@naver.com>
2021-12-02 21:27:35 +0900

    Update LoginResponse.kt

--- app/src/main/java/com/pipix/pipi/src/fragment/logged_out/login/model/LoginResponse.kt
+++ app/src/main/java/com/pipix/pipi/src/fragment/logged_out/login/model/LoginResponse.kt
@@ -1,5 +1,8 @@
 package com.pipix.pipi.src.fragment.logged_out.login.model
 
 data class LoginResponse (
-    val result : String
+    val email: String,
+    val id: Int,
+    val name: String,
+    val patients: List<Any>
         )

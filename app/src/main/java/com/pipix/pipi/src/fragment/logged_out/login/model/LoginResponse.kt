780ecae4e83766a5f9b482c02a9dfef922b1d5c7
changs97 <pck4949@naver.com>
2021-12-02 23:45:31 +0900

    Update LoginResponse.kt

--- app/src/main/java/com/pipix/pipi/src/fragment/logged_out/login/model/LoginResponse.kt
+++ app/src/main/java/com/pipix/pipi/src/fragment/logged_out/login/model/LoginResponse.kt
@@ -1,8 +1,14 @@
 package com.pipix.pipi.src.fragment.logged_out.login.model
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginResponse (
+    @SerializedName("email")
     val email: String,
+    @SerializedName("id")
     val id: Int,
+    @SerializedName("name")
     val name: String,
+    @SerializedName("patients")
     val patients: List<Any>
         )

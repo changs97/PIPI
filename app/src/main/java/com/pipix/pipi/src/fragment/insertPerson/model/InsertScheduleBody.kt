b922829f94bcb468300efd2cf6e52ee6af26473c
changs97 <pck4949@naver.com>
2021-12-03 01:07:57 +0900

    Create InsertScheduleBody.kt

---
+++ app/src/main/java/com/pipix/pipi/src/fragment/insertPerson/model/InsertScheduleBody.kt
@@ -0,0 +1,12 @@
+package com.pipix.pipi.src.fragment.insertPerson.model
+
+data class InsertScheduleBody(
+
+    val fri: String?,
+    val mon: String?,
+    val sat: String?,
+    val sun: String?,
+    val thu: String?,
+    val tue: String?,
+    val wed: String?
+)

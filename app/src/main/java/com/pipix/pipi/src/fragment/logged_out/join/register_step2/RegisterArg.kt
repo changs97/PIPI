7c97eb3a89cffc9eed744c2c5b5cc4bf7c8dde26
changs97 <pck4949@naver.com>
2021-12-02 21:26:49 +0900

    Create RegisterArg.kt

---
+++ app/src/main/java/com/pipix/pipi/src/fragment/logged_out/join/register_step2/RegisterArg.kt
@@ -0,0 +1,5 @@
+package com.pipix.pipi.src.fragment.logged_out.join.register_step2
+
+import java.io.Serializable
+
+data class RegisterArg (val userId : String, val password : String) : Serializable

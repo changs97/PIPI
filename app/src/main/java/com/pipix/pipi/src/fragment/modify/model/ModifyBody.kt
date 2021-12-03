ed7f7215bb861fe5b18d974971e1168ec1995cef
changs97 <pck4949@naver.com>
2021-12-03 18:55:04 +0900

    Update Data Class

---
+++ app/src/main/java/com/pipix/pipi/src/fragment/modify/model/ModifyBody.kt
@@ -0,0 +1,9 @@
+package com.pipix.pipi.src.fragment.modify.model
+
+data class ModifyBody(
+    val address: String,
+    val age: String,
+    val imageURL: String?,
+    val name: String,
+    val sex: Int
+)

d2bef83e7970acef331b5e7bf1e92a73a4fb23ee
changs97 <pck4949@naver.com>
2021-12-04 20:10:06 +0900

    Update Webservice.kt

--- app/src/main/java/com/pipix/pipi/data/Webservice.kt
+++ app/src/main/java/com/pipix/pipi/data/Webservice.kt
@@ -7,7 +7,6 @@ import com.pipix.pipi.src.fragment.insertPerson.model.InsertResponse
 import com.pipix.pipi.src.fragment.insertPerson.model.InsertScheduleBody
 import com.pipix.pipi.src.fragment.insertPerson.model.InsertScheduleResponse
 import com.pipix.pipi.src.fragment.logged_out.join.register_step2.model.SignUpBody
-import com.pipix.pipi.src.fragment.logged_out.join.register_step2.model.SignUpResponse
 import com.pipix.pipi.src.fragment.logged_out.login.model.LoginBody
 import com.pipix.pipi.src.fragment.logged_out.login.model.LoginResponse
 import com.pipix.pipi.src.fragment.modify.model.ModifyBody
